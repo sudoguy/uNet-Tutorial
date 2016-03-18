5490c23f8eeea615586351140eaebc4ae1922c48
EKemerov <eskemerov@gmail.com>
2016-03-18 12:48:48 +0700

    Added flying and camera rotation limit.

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -4,9 +4,13 @@
 public class PlayerMotor : MonoBehaviour {
 	
 	private Vector3 velocity = Vector3.zero;
-	private Vector3 cameraRotation = Vector3.zero;
+	private float cameraRotationX = 0f;
+	private float currentCameraRotationX = 0f;
 	private Vector3 rotation = Vector3.zero;
 	private Vector3 thrusterForce = Vector3.zero;
+
+	[SerializeField]
+	private float cameraRotationLimit = 85f;
 
 	[SerializeField]
 	private Camera cam;
@@ -31,9 +35,9 @@ public class PlayerMotor : MonoBehaviour {
 	}
 
 	// Gets a rotation vector for camera
-	public void RotateCamera(Vector3 _cameraRotation)
+	public void RotateCamera(float _cameraRotation)
 	{
-		cameraRotation = _cameraRotation;
+		cameraRotationX = _cameraRotation;
 	}
 
     // Get a force vector for our thrusters
@@ -70,7 +74,12 @@ public class PlayerMotor : MonoBehaviour {
 		rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
 		if (cam != null)
 		{
-			cam.transform.Rotate(-cameraRotation);
+			// Set out rotation and clamp it
+			currentCameraRotationX -= cameraRotationX;
+			currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+			// Apply out rotation to the transform of our camera
+			cam.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
 		}
 	}
 
